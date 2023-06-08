055d904bdece245e37a9a6aaaa91453d0dedec58
Marianprograma <mariano.arbeloa@gmail.com>
2023-06-08 20:40:18 -0300

    Correccion de politica

--- src/main/java/gida/simulators/labs/first/policies/MultipleServerSelectionPolicy.java
+++ src/main/java/gida/simulators/labs/first/policies/MultipleServerSelectionPolicy.java
@@ -1,5 +1,6 @@
 package gida.simulators.labs.first.policies;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gida.simulators.labs.first.entities.Entity;
@@ -15,77 +16,58 @@ import gida.simulators.labs.first.resources.Server;
 
 public class MultipleServerSelectionPolicy implements ServerSelectionPolicy {
 
+    public List<Server> filter(List<Server> servers, Entity entity) {
+        List<Server> servers2 = new ArrayList<>();
+
+        if (entity.getClass() == LightWeight.class) {
+            for (int i = 0; i < servers.size(); i++) {
+                if (servers.get(i).getClass() == AirstripL.class){
+                    servers2.add(servers.get(i));
+                }
+            }
+        }else if (entity.getClass() == Medium.class) {
+            for (int i = 0; i < servers.size(); i++) {
+                if (servers.get(i).getClass() == AirstripM.class){
+                    servers2.add(servers.get(i));
+                }
+            }
+        }else if (entity.getClass() == Heavy.class) {
+            for (int i = 0; i < servers.size(); i++) {
+                if (servers.get(i).getClass() == AirstripH.class){
+                    servers2.add(servers.get(i));
+                }
+            }
+        }
+        return servers2;
+    }
+
     @Override
     public Server selectServer(List<Server> servers, Entity entity) {
         
-        int serverTipo [] = new int[servers.size()];
-        serverTipo[0] = -1;
-        int j=0,i,minQ=0;
+        List<Server> sservers = filter(servers, entity);
+        int i,j,minQ=0;
 
-        if (entity.getClass() == LightWeight.class) {
-            for (i = 0; i < servers.size(); i++) {
-                if (servers.get(i).getClass() == AirstripL.class){
-                    if(servers.get(i).getCurrentEntity().getClass() != Maintenance.class && servers.get(i).getQueues().get(i).isUsable()){
-                        serverTipo[j] = i;
-                        j++;
+        for(i=0;i<sservers.size();i++){
+            if(!sservers.get(i).isBusy()){
+                return sservers.get(i);
+            }
+        }
+
+        System.out.println("i="+i);
+        if (sservers.get(i).getCurrentEntity().getClass() != Maintenance.class){
+            if(sservers.get(i) != null){
+                for (j = 0; j < (sservers.size())-1; j++) {
+                    if (sservers.get(minQ).getMaxSizeQueues() <= sservers.get(j+1).getMaxSizeQueues()){
+                        minQ = j;
+                    }else{
+                        minQ = j+1;
                     }
                 }
             }
-            if(serverTipo[0]<= 0){
-                for (i = 0; i < (serverTipo.length)-1; i++) {
-                    if (servers.get(serverTipo[minQ]).getMaxSizeQueues() <= servers.get(i+1).getMaxSizeQueues()){
-                        minQ = i;
-                    }else{
-                        minQ = i+1;
-                    }
-                }
-            }              
-            //en caso de no tener sv selecciona el -1 del arreglo serverTipo, el cual devuelve el server aux, 
-            //en muy pocos casos sucedera esto ya que nuestra politica y forma de programar las entidades no deja que se utilice.
-
-        } else if (entity.getClass() == Medium.class) {
-            for (i = 0; i < servers.size(); i++) {
-                if (servers.get(i).getClass() == AirstripM.class){
-                    if(servers.get(i).getCurrentEntity().getClass() != Maintenance.class && servers.get(i).getQueues().get(i).isUsable()){
-                        serverTipo[j] = i;
-                        j++;
-                    }
-                }
-            }
-            if(serverTipo[0]<= 0){
-                for (i = 0; i < (serverTipo.length)-1; i++) {
-                    if (servers.get(serverTipo[minQ]).getMaxSizeQueues() <= servers.get(i+1).getMaxSizeQueues()){
-                        minQ = i;
-                    }else{
-                        minQ = i+1;
-                    }
-                }
-            }  
-            
-        }else if (entity.getClass() == Heavy.class) {
-            for (i = 0; i < servers.size(); i++) {
-                if (servers.get(i).getClass() == AirstripH.class){
-                    if(servers.get(i).getCurrentEntity().getClass() != Maintenance.class && servers.get(i).getQueues().get(i).isUsable()){
-                        serverTipo[j] = i;
-                        j++;
-                    }
-                }
-            }
-            if(serverTipo[0]<= 0){
-                for (i = 0; i < (serverTipo.length)-1; i++) {
-                    if (servers.get(serverTipo[minQ]).getMaxSizeQueues() <= servers.get(i+1).getMaxSizeQueues()){
-                        minQ = i;
-                    }else{
-                        minQ = i+1;
-                    }
-                }
-            }  
-
-        }else {
-            //mantenimiento
+        }  
+        /*else {
             int var=0;
-            
-            for (i = 0; i < serverTipo.length-1; i++) {
+            for (int i = 0; i < servers.size()-1; i++) {
                 Airstrip a1 =  (Airstrip) servers.get(var);
                 Airstrip a2 = (Airstrip) servers.get(i+1);
                 if(a1.porcentajeWear() >= a2.porcentajeWear()){
@@ -94,12 +76,11 @@ public class MultipleServerSelectionPolicy implements ServerSelectionPolicy {
                     var=i+1;
                 }
             }
-            serverTipo[0] = var;
-            
-        }
-
-        return servers.get(serverTipo[minQ]+1);
+            servers2.add(servers.get(var));
+        }*/            
+        //en caso de no tener sv selecciona el -1 del arreglo serverTipo, el cual devuelve el server aux, 
+        //en muy pocos casos sucedera esto ya que nuestra politica y forma de programar las entidades no deja que se utilice.
+        return sservers.get(minQ);
     }
-
-    
 }
+
